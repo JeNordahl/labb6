fd1a56ac7b63f95b2344bef9a438e5412cc8b7e4
JeNordahl <jesper.nordahl@hotmail.com>
2024-05-14 11:27:36 +0200

    Lade till alla filer och api nyckel

---
+++ src/api/getMovieData.js
@@ -0,0 +1,16 @@
+const getMovieData = (movieTitle) => {
+    const url = `https://www.omdbapi.com/?s=${movieTitle}&type=movie&apikey=${process.env.NEXT_PUBLIC_OMDB_KEY}`;
+
+    const movieData = fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            return data.Search;
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+
+    return movieData;
+};
+
+export default getMovieData;
