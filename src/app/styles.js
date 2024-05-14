83a47d014ca8870b2fe9380d721ce2aec69c1f6f
JeNordahl <jesper.nordahl@hotmail.com>
2024-05-14 12:11:05 +0200

    Glömde styles.js

---
+++ src/app/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledMain = styled.main`
+  display: flex;
+  padding: 0 5rem;
+  flex-direction: row;
+  justify-content: space-evenly;
+  min-height: 100vh;
+  align-content: center;
+  text-align: left;
+`;
